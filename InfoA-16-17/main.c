0d08ceb65c8b1a277d60036c78e4fe1e30a50012
Francesco Marconi <franco.maroni@gmail.com>
2016-10-20 22:13:12 +0200

    Initial Commit

---
+++ InfoA-16-17/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  InfoA-16-17
+//
+//  Created by Francesco Marconi on 10/20/16.
+//  Copyright © 2016 Francesco Marconi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
