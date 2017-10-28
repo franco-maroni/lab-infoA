2ca275da0c3b483a159f7fd41795f5f35c9df589
Francesco Marconi <francesco.marconi@polimi.it>
2017-10-28 12:26:44 +0200

    aggiorna testo lab 1 e rinomina esercizi

--- Lab1/lab1es5.c
+++ Lab1/lab1es5.c
@@ -1,23 +1,40 @@
 /*
- ESERCIZIO 5 - Calcolo del fattoriale
- Scrivere un programma che, fornito in input un intero, ne restituisca in output il fattoriale n!.
+ ESERCIZIO 4 - Calcolo del massimo e del minimo
+ Scrivere un programma in C che, forniti in input N valori, ne determini il Massimo ed il minimo.
+ Il programma deve:
+ - chiedere in ingresso un valore intero corrispondente ad N
+ - chiedere in ingresso gli N valori (uno alla volta)
+ - stampare in output il valore massimo e minimo
+ 
  */
 
 #include <stdio.h>
 
 int main(void) {
-
-    int n, i, fact;
-    do {
-        printf("Inserire un valore intero positivo:\n");
-        scanf("%d", &n);
-    } while (n < 1);
-
+    int n, i, max = 0, min = 0, cur;
     
-    for(i = 0, fact = 1; i < n - 1; i++) {
-        fact *= n - i;
+    do {
+        printf("Inserire valore di N:\n");
+        scanf("%d", &n);
+    } while ( n < 1 );
+    
+    for(i = 0; i < n; i++) { // ripeto n volte
+        printf("Inserire valore numero %d:\n", i + 1);
+        scanf("%d", &cur); 
+        if (i == 0) { //alla prima iterazione, assegno lo stesso valore a max e min
+            min = cur;
+            max = cur;
+        } else{
+            if (cur < min) {
+                min = cur;
+            }
+            if (cur > max) {
+                max = cur;
+            }
+        }
     }
-
-    printf("Il fattoriale di %d è %d\n", n, fact);
+    printf("Il MASSIMO è %d\n", max);
+    printf("Il MINIMO è %d\n", min);
+    
     return 0;
 }
